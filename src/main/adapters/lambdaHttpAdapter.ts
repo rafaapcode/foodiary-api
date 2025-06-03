df7adb964aff6a6abf551d35c7a45b4620035185
rafaapcode <rafaap2003@gmail.com>
2025-06-03 08:44:47 -0300

    feat: validating the payload using zod

--- src/main/adapters/lambdaHttpAdapter.ts
+++ src/main/adapters/lambdaHttpAdapter.ts
@@ -1,22 +1,38 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+import { ZodError } from 'zod';
 import { IController } from '../../application/contracts/Controller';
 import { lambdaBodyParser } from '../utils/lambdaBodyParser';
 
-export function lambdaHttpAdapter(controller: IController<unknown>){
-  return async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
-    const body = lambdaBodyParser(event.body);
-    const params = event.pathParameters ?? {};
-    const queryParams = event.queryStringParameters ??{};
+export function lambdaHttpAdapter(controller: IController<unknown>) {
+  return async (
+    event: APIGatewayProxyEventV2,
+  ): Promise<APIGatewayProxyResultV2> => {
+    try {
+      const body = lambdaBodyParser(event.body);
+      const params = event.pathParameters ?? {};
+      const queryParams = event.queryStringParameters ?? {};
 
-    const response = await controller.handle({
-      body,
-      params,
-      queryParams,
-    });
+      const response = await controller.handle({
+        body,
+        params,
+        queryParams,
+      });
 
-    return {
-      statusCode: response.statusCode,
-      body: response.body ? JSON.stringify(response.body) : undefined,
-    };
+      return {
+        statusCode: response.statusCode,
+        body: response.body ? JSON.stringify(response.body) : undefined,
+      };
+    } catch (error) {
+      if(error instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: {
+            error: {
+              code: 'VALIDATION',
+            },
+          },
+        };
+      }
+    }
   };
 }
