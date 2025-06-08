36dcb2ecbf2fb113d0f4219e40608e6d1fa07ff0
rafaapcode <rafaap2003@gmail.com>
2025-06-08 14:35:30 -0300

    feat: handling erros of the signincontroller

--- src/application/usecases/auth/SignInUseCase.ts
+++ src/application/usecases/auth/SignInUseCase.ts
@@ -1,19 +1,37 @@
+import { InvalidCredentials } from '@application/errors/application/InvalidCredentials';
+import { NotAuthorizedException, UserNotFoundException } from '@aws-sdk/client-cognito-identity-provider';
 import { AuthGateway } from '@infra/gateways/AuthGateway';
 import { Injectable } from '@kernel/decorators/Injectable';
 
 @Injectable()
 export class SingInUseCase {
-  constructor(
-    private readonly authGateway: AuthGateway,
-  ) {}
+  constructor(private readonly authGateway: AuthGateway) {}
 
-  async execute({ email, password }: SingInUseCase.Input): Promise<SingInUseCase.OutPut> {
-    const { accessToken, refreshToken } = await this.authGateway.signIn({ email, password });
-
-    return {
-      accessToken,
-      refreshToken,
+  async execute({
+    email,
+    password,
+  }: SingInUseCase.Input): Promise<SingInUseCase.OutPut> {
+    const response = {
+      accessToken: '',
+      refreshToken: '',
     };
+
+    try {
+      const { accessToken, refreshToken } = await this.authGateway.signIn({
+        email,
+        password,
+      });
+      response.accessToken = accessToken;
+      response.refreshToken = refreshToken;
+    } catch (error) {
+      if(error instanceof UserNotFoundException || error instanceof NotAuthorizedException) {
+        throw new InvalidCredentials();
+      }
+
+      throw error;
+    }
+
+    return response;
   }
 }
 
