ac63333aced2aa59708170fcdc5965fa3a9c2309
rafaapcode <rafaap2003@gmail.com>
2025-06-16 18:02:14 -0300

    feat: sending the template as the message of the email

--- src/main/functions/auth/cognito/customMessageTrigger.ts
+++ src/main/functions/auth/cognito/customMessageTrigger.ts
@@ -1,11 +1,15 @@
+import ForgotPassword from '@infra/emails/templates/auth/ForgotPassword';
+import { render } from '@react-email/render';
 import { CustomMessageTriggerEvent } from 'aws-lambda';
 
 export async function  handler(event: CustomMessageTriggerEvent) {
   if(event.triggerSource === 'CustomMessage_ForgotPassword') {
-    const code = event.request.codeParameter;
+    const confirmationCode = event.request.codeParameter;
+
+    const html = await render(ForgotPassword({ confirmationCode }));
 
     event.response.emailSubject = '🍏 Foodiary | Recupere sua conta!';
-    event.response.emailMessage = `O seu código de recuperação é: ${code}`;
+    event.response.emailMessage = html;
   }
 
   return event;
