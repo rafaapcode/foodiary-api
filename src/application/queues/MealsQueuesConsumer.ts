417150ace8e938412f33d6a0e9e7b265b48240b6
rafaapcode <rafaap2003@gmail.com>
2025-08-17 14:57:45 -0300

    feat: processing the meal

--- src/application/queues/MealsQueuesConsumer.ts
+++ src/application/queues/MealsQueuesConsumer.ts
@@ -1,11 +1,14 @@
 import { IQueueConsumer } from '@application/contracts/IQueueConsumer';
+import { ProcessMealUseCase } from '@application/usecases/meals/ProcessMealUseCase';
 import { MealsQueueGateway } from '@infra/gateways/MealsQueueGateway';
 import { Injectable } from '@kernel/decorators/Injectable';
 
 @Injectable()
 export class MealsQueueConsumer implements IQueueConsumer<MealsQueueGateway.Message> {
+  constructor(private readonly processMealUseCase: ProcessMealUseCase) {}
+
   async process({ accountId, mealId }: MealsQueueGateway.Message): Promise<void> {
-    console.log(accountId, mealId);
+    await this.processMealUseCase.execute({ accountId, mealId });
   }
 
 }
---
+++ src/application/usecases/meals/ProcessMealUseCase.ts
@@ -0,0 +1,79 @@
+import { Meal } from '@application/entities/Meal';
+import { ResourceNotFound } from '@application/errors/application/ResourceNotFound';
+import { MealRepository } from '@infra/database/dynamo/repositories/MealRepository';
+import { MealsFileStorageGateway } from '@infra/gateways/MealsFileStorageGateway';
+import { Injectable } from '@kernel/decorators/Injectable';
+
+const MAX_ATTEMPTS = 2;
+
+@Injectable()
+export class ProcessMealUseCase {
+  constructor(
+    private readonly mealRepo: MealRepository,
+    private readonly mealFileStorageGw: MealsFileStorageGateway,
+  ) {}
+
+  async execute({
+    accountId,
+    mealId,
+  }: ProcessMealUseCase.Input): Promise<ProcessMealUseCase.OutPut> {
+    const meal = await this.mealRepo.findById({
+      mealId,
+      accountId,
+    });
+
+    if (!meal) {
+      throw new ResourceNotFound(`Meal "${mealId}" not found`);
+    }
+
+    if (meal.status === Meal.Status.UPLOADING) {
+      throw new Error(`Meal "${mealId}" is still uploading`);
+    }
+
+    if(meal.status === Meal.Status.PROCESSING) {
+      throw new Error(`Meal "${mealId}" is already being processing`);
+    }
+
+    if (meal.status === Meal.Status.SUCCESS) {
+      return;
+    }
+
+    try {
+      meal.status = Meal.Status.PROCESSING;
+      meal.attempts += 1;
+      await this.mealRepo.save(meal);
+
+      // processa com ia
+      meal.status = Meal.Status.SUCCESS;
+      meal.name = 'Teste';
+      meal.icon = '🥗';
+      meal.foods = [
+        {
+          calories: 100,
+          carbohydrates: 200,
+          fats: 300,
+          name: 'pao',
+          proteins: 200,
+          quantity: '2 unidades',
+        },
+      ];
+
+      await this.mealRepo.save(meal);
+    } catch (error) {
+      meal.status = meal.attempts >= MAX_ATTEMPTS ? Meal.Status.FAILED :Meal.Status.QUEUED;
+      await this.mealRepo.save(meal);
+
+      throw error;
+    }
+
+  }
+}
+
+export namespace ProcessMealUseCase {
+  export type Input = {
+    accountId: string;
+    mealId: string;
+  };
+
+  export type OutPut = void;
+}
